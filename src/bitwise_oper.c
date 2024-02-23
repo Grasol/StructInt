c1593271ec550be9cf64193eb766924bfa4c9181
Grasol <dirextxxx@Gmail.com>
2024-02-24 00:19:34 +0100

    first_bulid

--- src/bitwise_oper.c
+++ src/bitwise_oper.c
@@ -1,4 +1,19 @@
-
+/*
+ * This file is part of StructInt.
+ *
+ * StructInt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * StructInt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with StructInt.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 PyObject *structint_oper_and(PyObject *self, PyObject *b) {
